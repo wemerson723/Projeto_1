ec4a9aecf298f102d15ad8c87bb70bd93bda5ab7
wemerson723 <86853141+wemerson723@users.noreply.github.com>
2022-09-14 22:09:48 -0300

    Update Projeto.java

--- Projeto.java
+++ Projeto.java
@@ -1,89 +1,97 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-//import java.util.Data;
-//import java.text.SimpleDateFormat;
 
 class Projeto {  
         private String status;
         private int identificacao;
         private String descricao;
-        private Usuarios coordenador; 
+        private Usuarios cordenador; 
         private ArrayList<Usuarios> usuarios_ativos;
-        private ArrayList<String> atividades;
+        private ArrayList<Atividade> atividades;
         private double valor_bolsa;
-        private int periodo_virgencia;
+        private int periodo_vigencia;
         private Data data_inicio; 
         private Data data_termino;
 
-        private void Get_status(){
-            if((this.identificacao==null) && (this.descricao==null) && (this.coordenador)&& (this.usuarios_ativos)&& (this.atividades)&& (this.valor_bolsa)&& (this.periodo_virgencia)&& (this.data_inicio)&& (this.data_termino)){
-                this.status = 'Iniciado';
+
+
+        public String               Get_status(){               return status;                 }
+        public int                  Get_identificacao(){        return identificacao;          }
+        public String               Get_descricao(){            return descricao;              }
+        public Usuarios             Get_cordenador(){           return cordenador;             } 
+        public ArrayList<Usuarios>  Get_usuarios_ativos(){      return usuarios_ativos;        }
+        public Atividade            Get_atividade(int index){   return atividades.get(index);  }
+        public ArrayList<Atividade> Get_atividades(){           return atividades;             }
+        public double               Get_valor_bolsa(){          return valor_bolsa;            }
+        public int                  Get_periodo_vigencia(){    return periodo_vigencia;      }
+        public Data                 Get_data_inicio(){          return data_inicio;            }
+        public Data                 Get_data_termino(){         return data_termino;           } 
+
+
+
+        public void Edit_status           (String status) {                       this.status = status;                      }
+        public void Edit_identificacao    (int identificacao){                    this.identificacao = identificacao;        }
+        public void Edit_descricao        (String descricao){                     this.descricao = descricao;                }
+        public void Edit_cordenador       (Usuarios cordenador){                 this.cordenador = cordenador;               }
+        public void Edit_usuarios_ativos  (ArrayList<Usuarios> usuarios_ativos){  this.usuarios_ativos.add(new Usuarios());  }
+        public void Edit_valor_bolsa      (double valor_bolsa){                   this.valor_bolsa = valor_bolsa;            }
+        public void Edit_periodo_vigencia(int periodo_vigencia){                this.periodo_vigencia = periodo_vigencia;}
+        public void Edit_data_inicio      (){                                     this.data_inicio.Edit();                   }
+        public void Edit_data_termino     (){                                     this.data_termino.Edit();                  }
+
+
+
+        //public void Delete_identificacao(){     this.identificacao = 0;                          }
+        public void Delete_descricao(){         this.descricao = "";                             }
+        public void Delete_cordenador(){        this.cordenador = new Usuarios();                }
+        public void Delete_usuarios_ativos(){   this.usuarios_ativos = new ArrayList<Usuarios>();}
+        public void Delete_atividades(){        this.atividades = new ArrayList<Atividade>();    }
+        public void Delete_valor_bolsa(){       this.valor_bolsa = 0;                            }
+        public void Delete_periodo_vigencia(){ this.periodo_vigencia = 0;                      }
+        public void Delete_data_inicio(){       this.data_inicio = new Data();                   }
+        public void Delete_data_termino(){      this.data_termino = new Data();                  }
+
+
+
+        public void ADD_atividade   ()         {     this.atividades.add(new Atividade()) ;    }
+        public void Rem_atividade   (int index){     this.atividades.remove(index) ;           }
+
+
+        public int Check_atividade(int id){
+            for (int i = 0; i < atividades.size(); i++){
+                if(atividades.get(i).Get_identificacao() == id){
+                    return i;
+                }
             }
-            else{ this.status = 'Em processo de criação'; }
+            return -1;
         }
 
-        private int Get_identificacao(){ return this.identificacao; }
-        private int Get_descricao(){ return this.descricao; }
-        private int Get_coordenador(){ return this.descricao; } 
-        private int Get_usuarios_ativos(){ return this.usuarios_ativos; }
-        private int Get_atividades(){ return this.atividades; }
-        private int Get_valor_bolsa(){ return this.valor_bolsa; }
-        private int Get_periodo_virgencia(){ return this.periodo_virgencia; }
-        private int Get_data_inicio(){ return this.data_inicio; }
-        private int Get_data_termino(){ return this.data_termino; } 
-        
-        private int ADD_identificacao(int identificacao){  
-            this.identificacao = identificacao;}
-        private int ADD_descricao(String descricao){       
-            this.identificacao = identificacao;}
-        private int ADD_coordenador(Usuarios coordenador){ 
-            this.coordenador = coordenador;}
-        private int ADD_usuarios_ativos(ArrayList<Usuarios> usuarios_ativos){            
-            this.usuarios_ativos.add(new Usuarios());}
-        private int ADD_atividades(ArrayList<String> atividades){
-            this.atividades = atividades;}
-        private int ADD_valor_bolsa(double valor_bolsa){            
-            this.valor_bolsa = valor_bolsa;}
-        private int ADD_periodo_virgencia(int periodo_virgencia){            
-            this.periodo_virgencia = periodo_virgencia;}
-        private int ADD_data_inicio(Data data_inicio){  
-            this.data_inicio = data_inicio; }
-        private int ADD_data_termino(Data data_termino){ 
-            this.data_termino = data_termino; }
 
-        private int Delete_identificacao(){  
-            this.identificacao = identificacao;}
-        private int Delete_descricao(){       
-            this.identificacao = identificacao;}
-        private int Delete_coordenador(){ 
-            this.coordenador = coordenador;}
-        private int Delete_usuarios_ativos(){            
-            this.usuarios_ativos = usuarios_ativos;}
-        private int Delete_atividades(){
-            this.atividades = atividades;}
-        private int Delete_valor_bolsa(){            
-            this.valor_bolsa = valor_bolsa;}
-        private int Delete_periodo_virgencia(){            
-            this.periodo_virgencia = periodo_virgencia;}
-        private int Delete_data_inicio(){  
-            this.data_inicio = data_inicio; }
-        private int Delete_data_termino(){ 
-            this.data_termino = data_termino; }
-            
-        public Projeto(){
+        public boolean Check(){
+            if((this.status == "Em processo de criação.") && 
+               //(this.identificacao != -1)     &&
+               (this.descricao != "null")     &&
+               (this.atividades.size() != 0)  &&
+               (this.valor_bolsa != -1)       &&
+               (this.periodo_vigencia != -1) &&
+               (this.data_inicio.check())     &&
+               (this.data_termino.check()) ){
+                return false;
+            }
+            return true;
+        }            
+        public Projeto(Usuarios cordenador){
             Scanner ler = new Scanner(System.in);
+
+            this.status = "Em processo de criação.";
+            System.out.println("Digite uma identificação para o projeto.");
             this.identificacao = ler.nextInt();
-            this.descricao = ler.nextString();
-            this.coordenador = new Usuarios();
-            this.atividades = new atividade();
-            this.valor_bolsa = ler.nextDouble();
-            this.periodo_virgencia = ler.nextInt();
+            this.descricao = "null";
+            this.cordenador = cordenador;
+            this.atividades = new ArrayList<Atividade>();
+            this.valor_bolsa = -1;
+            this.periodo_vigencia = -1;
             this.data_inicio = new Data();
             this.data_termino = new Data();
         }
-       public static void main(String args[]) {  
-              
-       }
     }  
-
-     //  public Projeto(int identificacao, String descricao[], Usuarios coordenador, ArrayList<Usuarios> usuarios_ativos, ArrayList<String> atividades, double valor_bolsa, int periodo_virgencia, Data data_inicio, Data data_termino{
