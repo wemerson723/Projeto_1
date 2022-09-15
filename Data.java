dc27e6fa313fb138ec8407b305a6dd843b5d1c4c
wemerson723 <86853141+wemerson723@users.noreply.github.com>
2022-09-14 22:11:21 -0300

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,21 +1,52 @@
-    class Data{
+import java.util.Scanner;
 
-        int segundos;
-        int minutos;
-        int horas;
-        int dia;
-        int mes;
-        int ano;
+class Data{
 
-        public static int Data(int dia, int mes, int ano, int horas, int minutos, int segundos){
-            this.segundos = segundos;
-            this.minutos = minutos; 
-            this.horas = horas;
-            this.dia = dia;
-            this.mes = mes;
-            this.ano = ano;
-        }
-        public static void main(String args[]){
+    private int segundos;
+    private int minutos;
+    private int horas;
+    private int dia;
+    private int mes;
+    private int ano;
 
-        }
+    public int Get_segundos (){ return segundos; }
+    public int Get_minutos  (){ return minutos;  }
+    public int Get_horas    (){ return horas;    }
+    public int Get_dia      (){ return dia;      }
+    public int Get_mes      (){ return mes;      }
+    public int Get_ano      (){ return ano;      }
+
+    public boolean check(){ if ((segundos != -1) && (minutos != -1) && (horas != -1) && (dia != -1) && (mes != -1) && (ano != -1)){ return true; } else{ return false;}}
+        
+    public void Edit(){
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o Dia:");
+        this.dia = ler.nextInt();
+
+        System.out.println("Digite o Mês:");
+        this.mes = ler.nextInt();
+
+        System.out.println("Digite o Ano:");
+        this.ano = ler.nextInt();
+
+        System.out.println("Digite as Horas:");
+        this.horas = ler.nextInt();
+
+        System.out.println("Digite os Minutos:");
+        this.minutos = ler.nextInt();
+
+        System.out.println("Digite os Segundos:");
+        this.segundos = ler.nextInt();
+        
     }
+    public void data(){
+        this.segundos = -1;
+        this.minutos = -1; 
+        this.horas = -1;
+        this.dia = -1;
+        this.mes = -1;
+        this.ano = -1;
+    }
+}
